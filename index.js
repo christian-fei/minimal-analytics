cfe698a2719dea02527dbb9f6f20be073fbdd003
Christian Fei <crifei93@gmail.com>
2021-04-17 13:26:26 +0200

    add label to html

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function start (env = process.env, memory) {
   if (!options.SITE_BASE_URL) throw new Error('MISSING_SITE_BASE_URL')
 
   const CLIENT_JS = fs.readFileSync(path.resolve(__dirname, 'client.js'), 'utf-8').replace('{{STATS_BASE_URL}}', options.STATS_BASE_URL)
-  const INDEX_HTML = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8')
+  const INDEX_HTML = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8').replace(/\{\{SITE_BASE_URL\}\}/g, options.SITE_BASE_URL)
 
   memory = memory !== undefined ? memory : fs.readFileSync(options.DATA_PATH).toString('utf-8').split('\n').filter(Boolean).map(l => JSON.parse(l))
   const live = {}
