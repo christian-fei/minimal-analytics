82e2f86e557ddf87fed963f061712a3d09effe79
Christian Fei <crifei93@gmail.com>
2021-04-21 12:05:44 +0200

    static build and serving

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const http = require('http')
+const nodeStatic = require('node-static')
 const fs = require('fs')
 const path = require('path')
 const isBot = userAgent => /(bot|check|cloud|crawler|download|monitor|preview|scan|spider|google|qwantify|yahoo|HeadlessChrome)/i.test(userAgent)
@@ -27,8 +28,10 @@ async function start (env = process.env, memory) {
 
   if (env.MIGRATE) await migrate(options)
 
+  const file = new (nodeStatic.Server)(path.resolve(__dirname, 'client', 'build'))
+
   const CLIENT_JS = fs.readFileSync(path.resolve(__dirname, 'client.js'), 'utf-8').replace('{{STATS_BASE_URL}}', options.STATS_BASE_URL)
-  const INDEX_HTML = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8').replace(/\{\{SITE_BASE_URL\}\}/g, options.SITE_BASE_URL)
+  // const INDEX_HTML = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8').replace(/\{\{SITE_BASE_URL\}\}/g, options.SITE_BASE_URL)
 
   memory = memory !== undefined ? memory : fs.readFileSync(options.DATA_PATH).toString('utf-8').split('\n').filter(Boolean).map(l => JSON.parse(l))
   const live = {}
@@ -67,8 +70,10 @@ async function start (env = process.env, memory) {
       res.setHeader('Content-type', 'text/javascript')
       return res.end(CLIENT_JS)
     }
-    res.setHeader('Content-type', 'text/html')
-    return res.end(INDEX_HTML)
+    console.log('serve static', req.url)
+    return file.serve(req, res)
+    // res.setHeader('Content-type', 'text/html')
+    // return res.end(INDEX_HTML)
   })
 
   setInterval(() => {
