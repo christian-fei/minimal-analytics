97dbcdb2eca02f478152b34f540ac0678907d8c9
Christian Fei <crifei93@gmail.com>
2021-04-24 10:31:30 +0200

    extract analytics.getAll

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const fs = require('fs')
 const path = require('path')
 const isBot = userAgent => /(bot|check|cloud|crawler|download|monitor|preview|scan|spider|google|qwantify|yahoo|HeadlessChrome)/i.test(userAgent)
 const trackPageview = require('./lib/track-pageview')
-const parseFilters = require('./lib/parse-filters')
-const parseResolution = require('./lib/parse-resolution')
 const parseOptions = require('./lib/parse-options')
 const analytics = require('./lib/analytics')
 const migrate = require('./lib/migrate')
-const parseTimeframe = require('./lib/parse-timeframe')
 
 module.exports = {
   start
@@ -31,7 +28,6 @@ async function start (env = process.env, memory) {
   const file = new (nodeStatic.Server)(path.resolve(__dirname, 'client', 'build'))
 
   const CLIENT_JS = fs.readFileSync(path.resolve(__dirname, 'client.js'), 'utf-8').replace('{{STATS_BASE_URL}}', options.STATS_BASE_URL)
-  // const INDEX_HTML = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8').replace(/\{\{SITE_BASE_URL\}\}/g, options.SITE_BASE_URL)
 
   memory = memory !== undefined ? memory : fs.readFileSync(options.DATA_PATH).toString('utf-8').split('\n').filter(Boolean).map(l => JSON.parse(l))
   const live = {}
@@ -60,10 +56,10 @@ async function start (env = process.env, memory) {
       return res.end('' + Object.keys(live).length)
     }
     if (req.method === 'GET' && /^\/api/.test(req.url)) {
-      const analytics = getAnalytics(req.url, memory, live)
+      const result = analytics.getAll(req.url, memory, live)
       res.setHeader('Content-type', 'application/json')
       res.statusCode = 200
-      return res.end(JSON.stringify(analytics))
+      return res.end(JSON.stringify(result))
     }
     if (req.method === 'GET' && req.url === '/client.js') {
       res.setHeader('Content-type', 'text/javascript')
@@ -82,32 +78,4 @@ async function start (env = process.env, memory) {
   console.log(`listening on http://127.0.0.1:${options.HTTP_PORT}`)
   server.listen(options.HTTP_PORT)
   return { server, memory }
-}
-
-function getAnalytics (url, memory, live) {
-  const filters = parseFilters(url)
-  const { startDate } = parseTimeframe(url)
-  const resolution = parseResolution(url)
-
-  let data = memory.filter(m => +new Date(m.d) > startDate).reverse()
-  const keys = Object.keys(filters)
-  if (keys.length > 0) {
-    data = data.filter(d => keys.every(k => d[k] === filters[k]))
-  }
-  const pages = analytics.calculatePagesBreakdown(data, 25)
-  const referrers = analytics.calculateReferrersBreakdown(data, 25)
-  const chartData = analytics.calculateChartData(data, resolution)
-
-  const pageviewsCount = data.length
-  const visitorsCount = [...new Set(data.map(d => d.v))].length
-
-  return {
-    data: data.slice(0, 25),
-    pages,
-    referrers,
-    chartData,
-    pageviewsCount,
-    visitorsCount,
-    live: Object.keys(live).length
-  }
 }
