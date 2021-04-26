75ef54f8d38f630dd1232aa0854f482251510398
Christian Fei <crifei93@gmail.com>
2021-04-26 10:33:20 +0200

    show info message when not enough data is available to show cart

--- client/src/components/pageviews-chart.js
+++ client/src/components/pageviews-chart.js
@@ -1,6 +1,18 @@
 export default function ({ data, resolution }) {
   const chartMaxPageviews = Math.max(...data.chartData.map(d => d[1]))
 
+  if (data.chartData.length < 5) {
+    return (
+      <div id='pageviews-chart' style={{ 'text-align': 'center' }}>
+        Insufficient data to show chart..
+        <br />
+        <br />
+        :(
+      </div>
+    )
+  }
+
+  console.log('chartData.length', data.chartData.length)
   return (
     <div id='pageviews-chart'>
       <table class='charts-css column show-labels show-primary-axis'>
@@ -13,7 +25,7 @@ export default function ({ data, resolution }) {
 
         <tbody>
           {data.chartData.map((d, i) =>
-            <tr key={formatDate(d[0], resolution)}>
+            <tr key={d[0]}>
               <td style={{ '--start': i === 0 ? 0 : data.chartData[i - 1][1] / chartMaxPageviews, '--size': d[1] / chartMaxPageviews }}>
                 {data.chartData.length < 25 &&
                   <span class='data'>{d[1]}</span>}
