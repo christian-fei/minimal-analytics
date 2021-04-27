a30309c36e8447945d8fb652f55b7763ab69d3a3
Christian Fei <crifei93@gmail.com>
2021-04-27 16:47:37 +0200

    filter live

--- client/src/components/stats.js
+++ client/src/components/stats.js
@@ -7,6 +7,7 @@ export default function ({ data, filters, toggleFilter }) {
       return Object.assign(acc, { [key]: live[key] })
     }, {})
   }
+
   return (
     <div>
       <div class='grid contain'>
@@ -20,20 +21,20 @@ export default function ({ data, filters, toggleFilter }) {
         </div>
         <div class='w-50'>
           <h2>Live</h2>
-          <div id='live'>{Object.keys(data.live).length}</div>
+          <div id='live'>{Object.keys(live).length}</div>
         </div>
       </div>
-      {Object.keys(data.live).length > 0 &&
+      {Object.keys(live).length > 0 &&
         <div class='contain'>
           <h2>Live pages <span class='live-dot' /></h2>
           <ul>
-            {Object.keys(data.live).reduce((acc, curr) => {
-              const existing = acc.find(({ p }) => p === data.live[curr].pageview.p)
+            {Object.keys(live).reduce((acc, curr) => {
+              const existing = acc.find(({ p }) => p === live[curr].pageview.p)
               if (existing) {
                 existing.c++
                 return acc
               }
-              return acc.concat([{ p: data.live[curr].pageview.p, c: 1 }])
+              return acc.concat([{ p: live[curr].pageview.p, c: 1 }])
             }, []).map(({ p, c }) => <li class={`filterable ${filters.p === p && 'active'}`} onClick={() => toggleFilter('p', p)} key={p}>{c} &middot; {p}</li>)}
           </ul>
         </div>}
