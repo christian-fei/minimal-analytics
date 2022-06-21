13dc86f1f88c81bbb45cc316d9ef305223b992c4
Christian <crifei93@gmail.com>
2022-06-21 12:54:22 +0200

    refactor filters and timeframe selection

--- dashboard/components/filters.js
+++ dashboard/components/filters.js
@@ -5,94 +5,34 @@ export default function ({ updateTimeframe, updateCustomTimeframe, clearCustomTi
     (!filters.from && !filters.to) && h('div', { class: 'w-50-lg' }, [
       h('h4', {}, 'Timeframe'),
       h('div', {}, [
-        h('span', {
-          onClick: () => updateTimeframe('today'),
-          class: `select-timeframe filterable static-filter ${filters.timeframe === 'today' && 'active'}`,
-          name: 'today',
-          id: 'today'
-        }, 'Today'),
-        h('span', {
-          onClick: () => updateTimeframe('past-day'),
-          class: `select-timeframe filterable static-filter ${filters.timeframe === 'past-day' && 'active'}`,
-          name: 'past-day',
-          id: 'past-day'
-        }, 'Past day'),
-        h('span', {
-          onClick: () => updateTimeframe('past-week'),
-          class: `select-timeframe filterable static-filter ${filters.timeframe === 'past-week' && 'active'}`,
-          name: 'past-week',
-          id: 'past-week'
-        }, 'Past week')
+        timeframeSelection('today', 'Today'),
+        timeframeSelection('past-day', 'Past day'),
+        timeframeSelection('past-week', 'Past week')
       ]),
       h('div', {}, [
-        h('span', {
-          onClick: () => updateTimeframe('past-month'),
-          class: `select-timeframe filterable static-filter ${filters.timeframe === 'past-month' && 'active'}`,
-          name: 'past-month',
-          id: 'past-month'
-        }, 'Past month'),
-        h('span', {
-          onClick: () => updateTimeframe('past-6-months'),
-          class: `select-timeframe filterable static-filter ${filters.timeframe === 'past-6-months' && 'active'}`,
-          name: 'past-6-months',
-          id: 'past-6-months'
-        }, 'Past 6 months'),
-        h('span', {
-          onClick: () => updateTimeframe('past-year'),
-          class: `select-timeframe filterable static-filter ${filters.timeframe === 'past-year' && 'active'}`,
-          name: 'past-year',
-          id: 'past-year'
-        }, 'Past year'),
+        timeframeSelection('past-month', 'Past month'),
+        timeframeSelection('past-6-months', 'Past 6 months'),
+        timeframeSelection('past-year', 'Past year'),
       ])
     ]),
     (!filters.from && !filters.to) && h('div', { class: 'w-50-lg' }, [
       h('h4', {}, 'Resolution'),
       h('div', {}, [
         ['past-year'].includes(filters.timeframe) &&
-          h('span', {
-            onClick: () => updateResolution('monthly'),
-            class: `select-resolution filterable static-filter ${filters.resolution === 'monthly' && 'active'}`,
-            name: 'monthly',
-            id: 'monthly'
-          }, 'Monthly'),
+          resolutionSelection('monthly', 'Monthly'),
         ['past-week', 'past-month', 'past-6-months', 'past-year'].includes(filters.timeframe) &&
-            h('span', {
-              onClick: () => updateResolution('daily'),
-              class: `select-resolution filterable static-filter ${filters.resolution === 'daily' && 'active'}`,
-              name: 'daily',
-              id: 'daily'
-            }, 'Daily'),
+            resolutionSelection('daily', 'Daily'),
         ['today', 'past-day', 'past-week', 'past-month'].includes(filters.timeframe) &&
-            h('span', {
-              onClick: () => updateResolution('hourly'),
-              class: `select-resolution filterable static-filter ${filters.resolution === 'hourly' && 'active'}`,
-              name: 'hourly',
-              id: 'hourly'
-            }, 'Hourly'),
+            resolutionSelection('hourly', 'Hourly'),
         ['today', 'past-day'].includes(filters.timeframe) &&
-            h('span', {
-              onClick: () => updateResolution('minutes'),
-              class: `select-resolution filterable static-filter ${filters.resolution === 'minutes' && 'active'}`,
-              name: 'minutes',
-              id: 'minutes'
-            }, 'Minutes')
+            resolutionSelection('minutes', 'Minutes')
       ].filter(Boolean))
     ]),
     (filters.from || filters.to) && h('div', { class: 'w-50-lg' }, [
       h('h4', {}, 'Resolution'),
       h('div', {}, [
-        h('span', {
-          onClick: () => updateResolution('daily'),
-          class: `select-resolution filterable static-filter ${filters.resolution === 'daily' && 'active'}`,
-          name: 'daily',
-          id: 'daily'
-        }, 'Daily'),
-        h('span', {
-          onClick: () => updateResolution('hourly'),
-          class: `select-resolution filterable static-filter ${filters.resolution === 'hourly' && 'active'}`,
-          name: 'hourly',
-          id: 'hourly'
-        }, 'Hourly')
+        resolutionSelection('daily', 'Daily'),
+        resolutionSelection('hourly', 'Hourly')
       ])
     ]),
     filters.from && h('div', {}, [
@@ -124,4 +64,21 @@ export default function ({ updateTimeframe, updateCustomTimeframe, clearCustomTi
       'Clear [x]'
     ])
   ].filter(Boolean))
+
+  function timeframeSelection (timeframe, label) {
+    return h('span', {
+      onClick: () => updateTimeframe(timeframe),
+      class: `select-timeframe filterable static-filter ${filters.timeframe === timeframe && 'active'}`,
+      name: timeframe,
+      id: timeframe
+    }, label)
+  }
+  function resolutionSelection (resolution, label) {
+    return h('span', {
+      onClick: () => updateResolution(resolution),
+      class: `select-resolution filterable static-filter ${filters.resolution === resolution && 'active'}`,
+      name: resolution,
+      id: resolution
+    }, label)
+  }  
 }
