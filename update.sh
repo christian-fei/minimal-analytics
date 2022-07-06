2c4a7b3571e128466fe7144fe7b3e9e2d0502f5e
Christian <crifei93@gmail.com>
2022-07-06 12:57:35 +0200

    no need to stash

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git stash
 git pull --rebase
-git stash pop
 docker compose -f docker-compose.prod.yml stop
 docker compose -f docker-compose.prod.yml up -d --build --remove-orphans --renew-anon-volumes
