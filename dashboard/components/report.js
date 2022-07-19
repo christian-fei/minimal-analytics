f304f142da1ebf0901f33034da7d0d65a9a95ce4
Christian <crifei93@gmail.com>
2022-07-19 12:17:21 +0200

    report

--- dashboard/components/report.js
+++ dashboard/components/report.js
@@ -10,15 +10,15 @@ export default function ({ report, data, filters, loading }) {
         h('span', {}, 'Textual report')
       ]),
       h('h1', { class: '' }, `Stats for timeframe: ${filters.timeframe || (`${new Date(filters.from).toISOString()} - ${(filters.to && new Date(filters.to).toISOString()) || 'now'}`)}`),
-      h('h2', { class: '' }, 'The five most visited pages are'),
+      h('h2', { class: '' }, `${data.visitorsCount} visitors and ${data.pageviewsCount} pageviews`),
+      h('h2', { class: '' }, `Resulting in a ${(data.visitorsCount / data.pageviewsCount * 100).toFixed(0) + '%'} bounce rate`),
+      h('h2', { class: '' }, `Most traffic came from ${domain(data.referrers[0].r)} (${data.referrers[0].views}, ~${Math.floor(data.referrers[0].views / data.pageviewsCount * 100)}%)`),
+      h('h2', { class: '' }, 'The three most visited pages are'),
       h('ul', { class: '' }, [
         h('li', { class: '' }, `${data.pages[0].views} - ${data.pages[0].p}`),
         h('li', { class: '' }, `${data.pages[1].views} - ${data.pages[1].p}`),
-        h('li', { class: '' }, `${data.pages[2].views} - ${data.pages[2].p}`),
-        h('li', { class: '' }, `${data.pages[3].views} - ${data.pages[3].p}`),
-        h('li', { class: '' }, `${data.pages[4].views} - ${data.pages[4].p}`)
-      ]),
-      h('h2', { class: '' }, `Most traffic came from ${domain(data.referrers[0].r)} (${data.referrers[0].views}, ~${Math.floor(data.referrers[0].views / data.pageviewsCount * 100)}%)`)
+        h('li', { class: '' }, `${data.pages[2].views} - ${data.pages[2].p}`)
+      ])
     ])
   ])
 }
