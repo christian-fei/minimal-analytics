feeb81cec7ae33733ca46d45f69fc0a5382bb093
Christian <crifei93@gmail.com>
2022-10-21 09:12:27 +0200

    build(docker): force recreate

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 git pull --rebase
 docker compose -f docker-compose.prod.yml stop
-docker compose -f docker-compose.prod.yml up -d --build --remove-orphans --renew-anon-volumes
+docker compose -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans --renew-anon-volumes
