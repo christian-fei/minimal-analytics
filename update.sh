f7e2f567a34906af82ad49913e326dcb93559777
Christian <crifei93@gmail.com>
2022-10-21 09:16:23 +0200

    build(docker): prune images

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@
 
 git pull --rebase
 docker compose -f docker-compose.prod.yml stop
+docker image prune -f
 docker compose -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans --renew-anon-volumes
