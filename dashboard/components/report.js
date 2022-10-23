006cf36833ce84fc866b552ef65aec2c2ef4e395
Christian <crifei93@gmail.com>
2022-10-23 09:17:35 +0200

    fix(client): render views in textual report if available

--- dashboard/components/report.js
+++ dashboard/components/report.js
@@ -13,9 +13,9 @@ export default function ({ data, filters, loading }) {
       h('h2', { class: '' }, `Most traffic came from ${domainFromUrl(data.referrers[0].r)} (${data.referrers[0].views}, ~${Math.floor(data.referrers[0].views / data.pageviewsCount * 100)}%)`),
       h('h2', { class: '' }, 'The three most visited pages are'),
       h('ul', { class: '' }, [
-        h('li', { class: '' }, `${data.pages[0].views} - ${data.pages[0].p}`),
-        h('li', { class: '' }, `${data.pages[1].views} - ${data.pages[1].p}`),
-        h('li', { class: '' }, `${data.pages[2].views} - ${data.pages[2].p}`)
+        data.pages[0] && h('li', { class: '' }, `${data.pages[0].views} - ${data.pages[0].p}`),
+        data.pages[1] && h('li', { class: '' }, `${data.pages[1].views} - ${data.pages[1].p}`),
+        data.pages[2] && h('li', { class: '' }, `${data.pages[2].views} - ${data.pages[2].p}`)
       ])
     ])
   ])
