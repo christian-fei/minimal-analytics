0fed0a4cadfc6f035a4bcbd56c632cbb018fd3e4
Christian <crifei93@gmail.com>
2022-12-29 21:16:53 +0100

    top 5

--- dashboard/components/report.js
+++ dashboard/components/report.js
@@ -11,18 +11,22 @@ export default function ({ data, filters, loading }) {
       h('h2', { class: '' }, `${data.visitorsCount} visitors and ${data.pageviewsCount} pageviews`),
       h('h2', { class: '' }, `Resulting in a ${(data.visitorsCount / data.pageviewsCount * 100).toFixed(0) + '%'} bounce rate`),
       Array.isArray(data.referrers) && data.referrers.length > 0 && h('h2', { class: '' }, `Most traffic came from ${domainFromUrl(data.referrers[0].r)} (${data.referrers[0].views}, ~${Math.floor(data.referrers[0].views / data.pageviewsCount * 100)}%)`),
-      h('h2', { class: '' }, 'The three most visited pages are'),
+      h('h2', { class: '' }, `Top ${data.pages.length >= 5 ? 5 : data.pages.length} most visited pages are`),
       h('ul', { class: '' }, [
         data.pages[0] && h('li', { class: '' }, `${data.pages[0].views} - ${data.pages[0].p}`),
         data.pages[1] && h('li', { class: '' }, `${data.pages[1].views} - ${data.pages[1].p}`),
-        data.pages[2] && h('li', { class: '' }, `${data.pages[2].views} - ${data.pages[2].p}`)
-      ]),
-      h('h2', { class: '' }, 'The three most occurred events are'),
+        data.pages[2] && h('li', { class: '' }, `${data.pages[2].views} - ${data.pages[2].p}`),
+        data.pages[3] && h('li', { class: '' }, `${data.pages[3].views} - ${data.pages[3].p}`),
+        data.pages[4] && h('li', { class: '' }, `${data.pages[4].views} - ${data.pages[4].p}`),
+      ].filter(Boolean)),
+      h('h2', { class: '' }, `Top ${data.events.length >= 5 ? 5 : data.events.length} most occurred events are`),
       h('ul', { class: '' }, Array.isArray(data.events) && [
         data.events[0] && h('li', { class: '' }, `${data.events[0].views} - ${data.events[0].e}`),
         data.events[1] && h('li', { class: '' }, `${data.events[1].views} - ${data.events[1].e}`),
-        data.events[2] && h('li', { class: '' }, `${data.events[2].views} - ${data.events[2].e}`)
-      ]),
+        data.events[2] && h('li', { class: '' }, `${data.events[2].views} - ${data.events[2].e}`),
+        data.events[3] && h('li', { class: '' }, `${data.events[3].views} - ${data.events[3].e}`),
+        data.events[4] && h('li', { class: '' }, `${data.events[4].views} - ${data.events[4].e}`),
+      ].filter(Boolean)),
     ].filter(Boolean))
   ])
 }
