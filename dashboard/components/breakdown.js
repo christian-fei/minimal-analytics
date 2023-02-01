7ac6d80a7ac3331bbe872614fa21582d55dd4bac
Christian <crifei93@gmail.com>
2023-02-01 09:52:47 +0100

    events

--- dashboard/components/breakdown.js
+++ dashboard/components/breakdown.js
@@ -5,47 +5,70 @@ export default function ({ data, loading, filters = {}, toggleFilter }) {
   if (!data) return null
   const { referrers = [] } = data
   const { pages = [] } = data
+  const { events = [] } = data
+  const maxEvents = Math.max(...events.map(e => e.views))
   const maxReferrers = Math.max(...referrers.map(r => r.views))
   const maxPages = Math.max(...pages.map(r => r.views))
 
   return h('div', {}, [
-    h('div', { class: `grid-lg contain ${loading && 'loading'}` }, [
-      h('div', { class: 'w-50-lg', id: 'referrers' }, [
-        h('h2', {}, 'Top Referrers'),
-        h('ul', { id: 'top-referrers' }, referrers.map((d) => {
-          const favicon = `https://icons.duckduckgo.com/ip3/${domainFromUrl(d.r)}.ico`
+    h('div', {class: `${loading && 'loading'}`}, [
+      h('div', { class: `grid-lg contain` }, [
+        h('div', { class: 'w-50-lg', id: 'referrers' }, [
+          h('h2', {}, 'Top Referrers'),
+          h('ul', { id: 'top-referrers' }, referrers.map((d) => {
+            const favicon = `https://icons.duckduckgo.com/ip3/${domainFromUrl(d.r)}.ico`
+            return h('li', {
+              key: d.r,
+              class: `filterable ${filters.r === d.r && 'active'}`,
+              style: {
+                '--data-percentage': (100 - d.views * 80 / maxReferrers) + '%'
+              },
+              onClick: () => toggleFilter('r', d.r)
+            }, [
+              h('div', {}, [
+                h('b', { class: 'views' }, d.views),
+                h('img', { loading: 'lazy', class: 'favicon', src: favicon }, []),
+                d.r.replace('https://', '').replace('http://', '') || 'none'
+              ])
+            ])
+          }))
+        ]),
+        h('div', { class: 'w-50-lg', id: 'pages' }, [
+          h('h2', {}, 'Top Pages'),
+          h('ul', { id: 'top-pages' }, pages.map((d) => {
+            return h('li', {
+              key: d.p,
+              onClick: () => toggleFilter('p', d.p),
+              class: `filterable ${filters.p === d.p && 'active'}`,
+              style: {
+                '--data-percentage': (100 - d.views * 80 / maxPages) + '%'
+              }
+            }, [
+              h('b', { class: 'views' }, d.views),
+              d.p
+            ])
+          })
+          )
+        ])
+      ]),
+      h('div', { class: 'contain' }, [
+        h('h2', {}, 'Top Events'),
+        h('ul', { id: 'top-events' }, events.map((d) => {
           return h('li', {
-            key: d.r,
-            class: `filterable ${filters.r === d.r && 'active'}`,
+            key: d.e,
+            // class: `filterable ${filters.e === d.e && 'active'}`,
             style: {
-              '--data-percentage': (100 - d.views * 80 / maxReferrers) + '%'
+              '--data-percentage': (100 - d.views * 80 / maxEvents) + '%'
             },
-            onClick: () => toggleFilter('r', d.r)
+            // onClick: () => toggleFilter('e', d.e)
           }, [
             h('div', {}, [
               h('b', { class: 'views' }, d.views),
-              h('img', { loading: 'lazy', class: 'favicon', src: favicon }, []),
-              d.r.replace('https://', '').replace('http://', '') || 'none'
+              // h('img', { loading: 'lazy', class: 'favicon', src: favicon }, []),
+              d.e
             ])
           ])
         }))
-      ]),
-      h('div', { class: 'w-50-lg', id: 'pages' }, [
-        h('h2', {}, 'Top Pages'),
-        h('ul', { id: 'top-pages' }, pages.map((d) => {
-          return h('li', {
-            key: d.p,
-            onClick: () => toggleFilter('p', d.p),
-            class: `filterable ${filters.p === d.p && 'active'}`,
-            style: {
-              '--data-percentage': (100 - d.views * 80 / maxPages) + '%'
-            }
-          }, [
-            h('b', { class: 'views' }, d.views),
-            d.p
-          ])
-        })
-        )
       ])
     ])
   ])
