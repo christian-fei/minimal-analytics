a77f715701e0e62393ab207f60386857c7b5097c
Christian <crifei93@gmail.com>
2023-02-22 15:08:12 +0100

    build image in update.sh

--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@
 git pull --rebase
 docker compose -f docker-compose.prod.yml stop
 docker image prune -f
+docker compose -f docker-compose.prod.yml build --no-cache
 docker compose -f docker-compose.prod.yml up -d --build --force-recreate --remove-orphans --renew-anon-volumes
